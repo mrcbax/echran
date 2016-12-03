826f65f9d7b547debe12f3c5e47e91d3d3e464fc
LogoiLab <cbaxter@mail.umw.edu>
2016-12-03 17:37:01 -0500

    5:30pm

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,11 @@ extern crate tempfile;
 
 use clap::{App, Arg};
 use ears::{AudioController, Recorder, Sound};
-use tempfile::NamedTempFileOptions;
+//use tempfile::NamedTempFileOptions;
 
-use std::io::{self, BufReader};
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
+use std::process::Command;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
 
@@ -26,7 +24,7 @@ fn main() {
         .required(true)
         .short("r")
         .takes_value(true)
-        .help("The number of times to play the a tone (default: 1)")
+        .help("Length of recording in milliseconds.")
     )
     .arg(Arg::with_name("tone_file")
         .index(2)
@@ -37,16 +35,16 @@ fn main() {
     )
     .get_matches();
 
-//create tempfile
+    /*//create tempfile
     let named_temp_file = NamedTempFileOptions::new()
-        .prefix("echran")
-        .suffix(".wav")
-        .create()
-        .unwrap();
+    .prefix("echran")
+    .suffix(".wav")
+    .create()
+    .unwrap();
     //get tempfile path
     let name = named_temp_file.path()
-        .file_name().unwrap();
-    let nameStr = name.to_os_string().into_string().unwrap();
+    .file_name().unwrap();
+    let nameStr = name.to_os_string().into_string().unwrap();*/
 
     // Create a Sound with the path of the sound file.
     let mut snd = Sound::new(m.value_of("tone_file").unwrap()).unwrap();
@@ -56,41 +54,36 @@ fn main() {
     // Create a new context to record audio
     let context = ears::init_in().unwrap();
     // Create the recorder
-    let mut recorder = Recorder::new(context);
-    while snd.is_playing() {
-        // Start to record the tone
-        recorder.start();
-    }
-    // Stop the recorder
+    let recorder = Recorder::new(context);
+    // Start to record the tone
+    ears::init();
+
+    // initialize the RecordContext
+    let ctxt = ears::init_in().expect("Initialization error!");
+
+    // Create a new Recorder using the RecordContext
+    let mut recorder = ears::Recorder::new(ctxt);
+    println!("Recording for {} seconds", m.value_of("randomness").unwrap());
+    recorder.start();
+    let mils = m.value_of("randomness").unwrap().parse::<u64>();
+    sleep(Duration::from_millis(mils.unwrap()));
     recorder.stop();
+    match recorder.save_to_file("echran") {
+        true => println!("Save okay!"),
+        false => println!("Cannot save ...")
+    }
+
     // Then store the recorded data in a file
-    let chars_to_trim: &[char] = &['.', 'w', 'a', 'v'];
-    let trimmed_str: &str = nameStr.as_str().trim_matches(chars_to_trim);
-    recorder.save_to_file(trimmed_str);
+    //let chars_to_trim: &[char] = &['.', 'w', 'a', 'v'];
+    //let trimmed_str: &str = nameStr.as_str().trim_matches(chars_to_trim);
+    let output = Command::new("sh")
+    .arg("-c")
+    .arg("md5sum echran.wav")
+    .output()
+    .expect("failed to execute process");
 
-    let path = Path::new("hello.txt");
-    let display = path.display();
-
-    // Open the path in read-only mode, returns `io::Result<File>`
-    let mut file = match File::open(&path) {
-        // The `description` method of `io::Error` returns a string that
-        // describes the error
-        Err(why) => panic!("couldn't open {}: {}", display,
-                                                   why.description()),
-        Ok(file) => file,
-    };
-    let mut bytes: Vec<u8>;
-    match file.read(&mut bytes) {
-        Err(why) => panic!("couldn't read {}: {}", display,
-                                                   why.description()),
-        Ok(_) => print!("{}", display),
+    let md5 = output.stdout;
+    for i in 0..md5.len() {
+        println!("RandNum: {}", md5[i]);
     }
-
-    let mut m = sha1::Sha1::new();
-    for i in 0..bytes.len() {
-        let temp: [u8] = [bytes.get(i).unwrap()];
-        m.update(temp);
-    }
-
-    println!("{}", m.digest().to_string());
 }

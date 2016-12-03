8476e0e550d153adace6ffc1bf6fe1894b0b1cd7
LogoiLab <cbaxter@mail.umw.edu>
2016-12-03 12:30:00 -0500

    12:30

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,55 @@
 extern crate clap;
 extern crate ears;
 extern crate rand;
+extern crate tempfile;
+extern crate ssdeep;
 
 use clap::{App, Arg};
+use ears::{Sound, SoundData, AudioController};
 use rand::{Rng, SeedableRng, StdRng};
-use ears::{AudioController, Record, Sound};
+use tempfile::NamedTempFileOptions;
+
+use std::fs::File;
+use std::rc::Rc;
+use std::cell::RefCell;
 
 fn main() {
-    let mut ran = rand::thread_rng();
+
     let m = App::new("Echo Randomizer")
-    .author(crate_authors!())
-    .version(crate_version!())
-    .about("A Proof of Concept truly random number generator.")
-    .arg(Arg::with_name("randomness")
+        .author(crate_authors!())
+        .version(crate_version!())
+        .about("A Proof of Concept truly random number generator.")
+        .arg(Arg::with_name("randomness")
         .index(1)
+        .required(true)
         .short("r")
-        .help("The length of time in seconds to use as seed (default: 2)")
+        .takes_value(true)
+        .help("The number of times to play the a tone (default: 1)")
+    )
+    .arg(Arg::with_name("tone_file")
+        .index(2)
+        .required(true)
+        .short("t")
+        .takes_value(true)
+        .help("The path to an ogg tone file to play.")
     )
     .get_matches();
-    println!("i32: {}", ran.gen::<i32>());
+
+    let named_temp_file = NamedTempFileOptions::new()
+        .prefix("tempile")
+        .suffix(".temp")
+        .create()
+        .unwrap();
+    let name = named_temp_file.path()
+        .file_name().unwrap();
+    // Create a Sound with the path of the sound file.
+    let mut snd = Sound::new(m.value_of("tone_file").unwrap()).unwrap();
+
+    // Play it
+    snd.play();
+
+    // Wait until the sound stopped playing
+    while snd.is_playing() {}
+    let h = ssdeep::hash_from_file(name).unwrap();
+    println!("ssdeep: {}", h);
 }

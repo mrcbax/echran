328c2623a8c74de0388980cbf02e260423de3bb1
LogoiLab <cbaxter@mail.umw.edu>
2016-12-03 13:08:30 -0500

    1:00pm

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ extern crate tempfile;
 extern crate ssdeep;
 
 use clap::{App, Arg};
-use ears::{Sound, SoundData, AudioController};
-use rand::{Rng, SeedableRng, StdRng};
+use ears::{AudioController, Recorder, Sound};
+//use rand::{Rng, SeedableRng, StdRng};
 use tempfile::NamedTempFileOptions;
 
-use std::fs::File;
-use std::rc::Rc;
-use std::cell::RefCell;
+//use std::fs::File;
+//use std::rc::Rc;
+//use std::cell::RefCell;
 
 fn main() {
 
@@ -36,21 +36,33 @@ fn main() {
     )
     .get_matches();
 
+    //create tempfile
     let named_temp_file = NamedTempFileOptions::new()
-        .prefix("tempile")
+        .prefix("echran")
         .suffix(".temp")
         .create()
         .unwrap();
+    //get tempfile path
     let name = named_temp_file.path()
         .file_name().unwrap();
     // Create a Sound with the path of the sound file.
     let mut snd = Sound::new(m.value_of("tone_file").unwrap()).unwrap();
-
     // Play it
     snd.play();
 
-    // Wait until the sound stopped playing
-    while snd.is_playing() {}
+    // Create a new context to record audio
+    let context = ears::init_in().unwrap();
+    // Create the recorder
+    let mut recorder = Recorder::new(context);
+    while snd.is_playing() {
+        // Start to record the tone
+        recorder.start();
+    }
+    // Stop the recorder
+    recorder.stop();
+    // Then store the recorded data in a file
+    recorder.save_to_file(name.to_os_string().into_string().unwrap().as_str());
     let h = ssdeep::hash_from_file(name).unwrap();
+
     println!("ssdeep: {}", h);
 }
